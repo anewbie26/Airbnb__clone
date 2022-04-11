5d4bcbc036e96c6d56cf4a5a789656bf5f54e1fc
ashfaque khan <ashfaquek036@gmail.com>
2022-04-11 12:24:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>airbnb clone</h1>
+      <Home />
+{/* Home */}
+{/* Header */}
+{/* Banner */}
+{/* Cards */}
+{/* Footer */}
+{/* SearchPage */}
+{/* ..... */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css';
+function Home() {
+  return (
+    <div>
+ this is Home Pgae     
+    </div>
+  )
+}
+export default Home
